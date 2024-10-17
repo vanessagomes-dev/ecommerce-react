0594e0534b299be8653edb9c4c8fae1cf441f359
vanessa <vanessagomesdev@gmail.com>
2024-10-17 19:02:02 -0300

    criacao pasta frontend react

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bem-vindo ao E-commerce!</h1>
+      <p>Em breve, você verá a lista de produtos aqui!</p>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h2>E-commerce</h2>
+      <ul>
+        <li>Home</li>
+        <li>Produtos</li>
+        <li>Carrinho</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
